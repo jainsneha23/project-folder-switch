83e6f22fd8188a11bbf25ba8907c181988c39834
Jain, Sneha T <Sneha.T.Jain@in.tesco.com>
2019-01-02 23:49:17 +0530

    Initial commit:

---
+++ spec/tree-view-switch-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TreeViewSwitchView from '../lib/tree-view-switch-view';
+
+describe('TreeViewSwitchView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
